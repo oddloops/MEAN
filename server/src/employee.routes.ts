43251505e4e6200cd84962d53fc745bbe28bf535
Vinci Wu <wu.vinci00@gmail.com>
2024-10-01 17:23:14 -0400

    Implement GET endpoints to get employees from mongoDB database

---
+++ server/src/employee.routes.ts
@@ -0,0 +1,33 @@
+import * as express from "express";
+import { ObjectId } from "mongodb";
+import { collections } from "./database";
+
+export const employeeRouter = express.Router();
+employeeRouter.use(express.json());
+
+// Get all employees for route /employees
+employeeRouter.get("/", async (_req, res) => {
+    try {
+        const employees = await collections?.employees?.find({}).toArray();
+        res.status(200).send(employees);
+    } catch (error) {
+        res.status(500).send(error instanceof Error ? error.message : "Unknown error");
+    }
+});
+
+// Get employee based on ID
+employeeRouter.get("/:id", async (req, res) => {
+    try {
+        const id = req?.params?.id;
+        const query = { _id: new ObjectId(id) };
+        const employee = await collections?.employees?.findOne(query);
+
+        if (employee) {
+            res.status(200).send(employee);
+        } else {
+            res.status(404).send(`Failed to find an employee: ID ${id}`);
+        }
+    } catch (error) {
+        res.status(404).send(`Failed to find an employee: ID ${req?.params?.id}`);
+    }
+});
