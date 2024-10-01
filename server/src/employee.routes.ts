9a07a35375289f356e6e0e2331d85540ba379773
Vinci Wu <wu.vinci00@gmail.com>
2024-10-01 17:55:56 -0400

    Implemented POST, PUT, and DELETE endpoints

--- server/src/employee.routes.ts
+++ server/src/employee.routes.ts
@@ -31,3 +31,62 @@ employeeRouter.get("/:id", async (req, res) => {
         res.status(404).send(`Failed to find an employee: ID ${req?.params?.id}`);
     }
 });
+
+// POST endpoint to create a new employee
+employeeRouter.post("/", async (req, res) => {
+    try {
+        const employee = req.body;
+        const result = await collections?.employees?.insertOne(employee);
+        if (result?.acknowledged) {
+            res.status(201).send(`Created a new employee: ID ${result.insertedId}.`);
+        } else {
+            res.status(500).send("Failed to create a new employee.");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error instanceof Error ? error.message : "Unknown error");
+    }
+});
+
+// PUT endpoint to update an existing employee
+employeeRouter.put("/:id", async (req, res) => {
+    try {
+        const id = req?.params?.id;
+        const employee = req.body;
+        const query = { _id: new ObjectId(id) };
+        const result = await collections?.employees?.updateOne(query, { $set: employee });
+
+        if (result && result.matchedCount) {
+            res.status(200).send(`Updated an employee: ID ${id}.`);
+        } else if (!result?.matchedCount) {
+            res.status(404).send(`Failed to find an employee: ID ${id}`);
+        } else {
+            res.status(304).send(`Failed to update an employee: ID ${id}`);
+        }
+    } catch (error) {
+        const message = error instanceof Error ? error.message : "Unknown error";
+        console.error(message);
+        res.status(400).send(message);
+    }
+});
+
+// DELETE enpoint to remove an existing employee
+employeeRouter.delete("/:id", async (req, res) => {
+    try {
+        const id = req?.params?.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await collections?.employees?.deleteOne(query);
+
+        if (result && result.deletedCount) {
+            res.status(202).send(`Removed an employee: ID ${id}`);
+        } else if (!result) {
+            res.status(400).send(`Failed to remove an employee: ID ${id}`);
+        } else if (!result.deletedCount) {
+            res.status(404).send(`Failed to find an employee: ID ${id}`);
+        }
+    } catch (error) {
+        const message = error instanceof Error ? error.message : "Unknown error";
+        console.error(message);
+        res.status(400).send(message);
+    }
+});
